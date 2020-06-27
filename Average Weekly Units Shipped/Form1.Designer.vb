9a864ccb643fe9d71ccb4e8ce736b1adced2a51f
16479 <16479@SURFACE>
2020-06-27 00:55:17 -0400

    Add project files.

---
+++ Average Weekly Units Shipped/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAverageWeeklyUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtUnits = New System.Windows.Forms.TextBox()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.txtGiantBox = New System.Windows.Forms.TextBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(70, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 46)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Units: "
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtUnits
+        '
+        Me.txtUnits.Location = New System.Drawing.Point(260, 46)
+        Me.txtUnits.Name = "txtUnits"
+        Me.txtUnits.Size = New System.Drawing.Size(105, 29)
+        Me.txtUnits.TabIndex = 1
+        Me.txtUnits.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblDay
+        '
+        Me.lblDay.Location = New System.Drawing.Point(442, 38)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(105, 46)
+        Me.lblDay.TabIndex = 2
+        Me.lblDay.Text = "Day 1"
+        Me.lblDay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.BackColor = System.Drawing.Color.White
+        Me.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAnswer.ForeColor = System.Drawing.Color.Black
+        Me.lblAnswer.Location = New System.Drawing.Point(173, 448)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(310, 45)
+        Me.lblAnswer.TabIndex = 4
+        '
+        'txtGiantBox
+        '
+        Me.txtGiantBox.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.txtGiantBox.ForeColor = System.Drawing.Color.White
+        Me.txtGiantBox.Location = New System.Drawing.Point(138, 124)
+        Me.txtGiantBox.Multiline = True
+        Me.txtGiantBox.Name = "txtGiantBox"
+        Me.txtGiantBox.ReadOnly = true
+        Me.txtGiantBox.Size = New System.Drawing.Size(376, 268)
+        Me.txtGiantBox.TabIndex = 3
+        '
+        'btnEnter
+        '
+        Me.btnEnter.BackColor = System.Drawing.Color.Black
+        Me.btnEnter.Location = New System.Drawing.Point(90, 536)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(126, 63)
+        Me.btnEnter.TabIndex = 5
+        Me.btnEnter.Text = "&Enter"
+        Me.btnEnter.UseVisualStyleBackColor = false
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.Black
+        Me.btnReset.Location = New System.Drawing.Point(271, 536)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(126, 63)
+        Me.btnReset.TabIndex = 6
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = false
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Black
+        Me.btnExit.Location = New System.Drawing.Point(447, 536)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(126, 63)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = false
+        '
+        'frmAverageWeeklyUnitsShipped
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11!, 24!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(32,Byte),Integer), CType(CType(32,Byte),Integer), CType(CType(32,Byte),Integer))
+        Me.ClientSize = New System.Drawing.Size(655, 655)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.txtGiantBox)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.txtUnits)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.MaximizeBox = false
+        Me.MinimizeBox = false
+        Me.Name = "frmAverageWeeklyUnitsShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Weekly Units Shipped"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtUnits As TextBox
+    Friend WithEvents lblDay As Label
+    Friend WithEvents lblAnswer As Label
+    Friend WithEvents txtGiantBox As TextBox
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+
+
+End Class
---
+++ Average Weekly Units Shipped/Form1.vb
@@ -0,0 +1,83 @@
+﻿'Name: Nirmal Patel
+'SID: 100767993
+'File Name: Average Weekly Units Shipped
+'Submitted to Austin Garrod
+'Date: 30th May 2020
+
+Public Class frmAverageWeeklyUnitsShipped
+
+    'Declarations
+    Dim unitsInput(6) As Integer    'unitInput is declared as an integer and is given an array 6
+    Dim averageUnits As Double      'averageUnits is declared as a double
+    Dim changeOfNum As Integer      'changeOfNum is declared as an integer
+
+    ''' <summary>
+    ''' When pressed btnEnter
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        txtUnits.SelectAll()        'select all content in txtUnit textbox
+        txtUnits.Focus()        'focus on txtUnits textbox
+        If Not IsNumeric(Me.txtUnits.Text) Then     'if input is not a number
+            MsgBox("Please enter a numeric value for Units")    'message
+            txtUnits.SelectAll()        'select all content in txtUnit textbox
+            txtUnits.Focus()        'focus on txtUnits textbox
+
+        Else
+            If Not Integer.TryParse(txtUnits.Text, unitsInput(changeOfNum)) Then    'if user input is not a whole number
+                MsgBox("Input must be a whole number")     'message
+                txtUnits.SelectAll()        'select all content in txtUnit textbox
+                txtUnits.Focus()        'focus on txtUnits textbox
+
+
+            Else
+                If Not unitsInput(changeOfNum) > 0 And unitsInput(changeOfNum) < 5000 Then
+                    MsgBox("Please enter a valid number between 0 and 5000")       'message
+                    txtUnits.SelectAll()        'select all content in txtUnit textbox
+                    txtUnits.Focus()        'focus on txtUnits textbox
+                Else
+
+                    txtGiantBox.Text += txtUnits.Text + vbCrLf      'print user input in txtGiantBox textbox
+                    txtUnits.Clear()    'clear all content in txtUnits textbox
+                    changeOfNum += 1    'increase 1 num
+                    lblDay.Text = "Day " + Convert.ToString(changeOfNum + 1)    'reset lblDay to Day 1
+                End If
+
+            End If
+
+        End If
+
+        'process (magical formula)
+        averageUnits = unitsInput.Average
+
+        'output
+        lblAnswer.Text = "Average per day: " + Convert.ToString(Math.Round((averageUnits), 2))
+
+        txtUnits.SelectAll()
+        txtUnits.Focus()
+
+    End Sub
+
+    ''' <summary>
+    ''' when prssed btnExit
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()  'exit the app
+    End Sub
+
+    ''' <summary>
+    ''' when pressed btnReset
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        lblAnswer.Text = ""     'in short, it will clear the content in lblAnswer
+        lblDay.Text = "Day 1"       'reset lblDay text to Day 1 after all
+        txtUnits.Clear()    'clear all content in txtUnits textbox
+        txtUnits.Focus()        'focus on txtUnits textbox
+    End Sub
+End Class
